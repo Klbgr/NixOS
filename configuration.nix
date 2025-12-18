9a7d2134ce629221db29cbe05020543418ae1286
Klbgr <qiuantoine@gmail.com>
2025-12-19 00:03:41 +0100

    Added automatic storage optimization

--- configuration.nix
+++ configuration.nix
@@ -89,6 +89,15 @@ in
 
   time.hardwareClockInLocalTime = true;
 
+  nix = {
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave

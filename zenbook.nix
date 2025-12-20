390fed83d1e373a72fbdf8b1fc586196a9a1db3d
Klbgr <qiuantoine@gmail.com>
2025-12-20 10:58:16 +0100

    Calibrated touchpad

--- zenbook.nix
+++ zenbook.nix
@@ -28,4 +28,12 @@
       Group = "ydotool";
     };
   };
+
+  services.udev.extraHwdb = ''
+    evdev:name:GDX1515:00 27C6:01F4 Touchpad:dmi:*svnASUSTeKCOMPUTERINC.:*pnZenBookUX434FL_UX434FL**
+      EVDEV_ABS_00=::60
+      EVDEV_ABS_01=::60
+      EVDEV_ABS_35=::60
+      EVDEV_ABS_36=::60
+  '';
 }

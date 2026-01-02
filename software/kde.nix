75cc79dcf96fd8d09dd6a7f7af2b45c9e1ccb7e3
Klbgr <qiuantoine@gmail.com>
2026-01-02 21:57:06 +0100

    Removed xdg.portal

--- software/kde.nix
+++ software/kde.nix
@@ -1,12 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
-    config.common.default = "kde";
-  };
-
   environment.systemPackages = with pkgs; [
     (where-is-my-sddm-theme.override {
       themeConfig.General = {

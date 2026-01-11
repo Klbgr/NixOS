9c88878b43a761e39fd8a34372d1203a8df0c3c0
Klbgr <qiuantoine@gmail.com>
2026-01-11 01:17:25 +0100

    Fixed FN lock on boot

--- zenbook.nix
+++ zenbook.nix
@@ -11,11 +11,7 @@
 
   networking.hostName = "ZenBook";
 
-#   services.udev.extraHwdb = ''
-#     evdev:name:GDX1515:00 27C6:01F4 Touchpad:dmi:*svnASUSTeKCOMPUTERINC.:*pnZenBookUX434FL_UX434FL**
-#       EVDEV_ABS_00=::60
-#       EVDEV_ABS_01=::60
-#       EVDEV_ABS_35=::60
-#       EVDEV_ABS_36=::60
-#   '';
+  boot.extraModprobeConfig = ''
+    options asus_wmi fnlock_default=0
+  '';
 }

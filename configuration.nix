2f718e0fa3ccf7d9df62ec55e10747a8889f2150
Klbgr <qiuantoine@gmail.com>
2026-01-30 23:33:43 +0100

    Added SMB mounts

--- configuration.nix
+++ configuration.nix
@@ -108,6 +108,8 @@
   };
   security.polkit.enable = true;
 
+  services.avahi.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- users/antoine/configuration.nix
+++ users/antoine/configuration.nix
@@ -1,8 +1,11 @@
 { config, pkgs, ... }:
-
+let
+  uid = 1000;
+in
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.antoine = {
+    uid = uid;
     isNormalUser = true;
     description = "Antoine";
     extraGroups = [
@@ -13,6 +16,37 @@
       "ydotool"
     ];
   };
+
+  fileSystems =
+    let
+      commonOptions = [
+        "x-systemd.automount"
+        "noauto"
+        "x-systemd.idle-timeout=60"
+        "credentials=/etc/nixos/users/antoine/smb-secrets"
+        "uid=${builtins.toString uid}"
+        "gid=100"
+        "dir_mode=0700"
+        "file_mode=0600"
+        "nofail"
+      ];
+    in
+    builtins.listToAttrs (
+      map
+        (name: {
+          name = "/mnt/antoine/${name}";
+          value = {
+            device = "//192.168.0.4/${name}";
+            fsType = "cifs";
+            options = commonOptions;
+          };
+        })
+        [
+          "data"
+          "docker"
+          "home_assistant"
+        ]
+    );
 
   home-manager.users.antoine =
     { pkgs, ... }:

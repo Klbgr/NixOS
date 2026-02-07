3de0a05b1c75b2aa01212c30ade986c40fc7178c
Klbgr <qiuantoine@gmail.com>
2026-02-07 22:08:17 +0100

    Disabled KWallet

--- users/antoine/home-manager/kde.nix
+++ users/antoine/home-manager/kde.nix
@@ -24,7 +24,6 @@ in
   home.packages = with pkgs; [
     kdePackages.qtstyleplugin-kvantum
     kdePackages.dynamic-workspaces
-    # kdePackages.koi
     burn-my-windows-kwin
   ];
 
@@ -62,7 +61,7 @@ in
           kwin6_effect_aura_glowEnabled = false;
         };
         kded5rc.Module-browserintegrationreminder.autoload = false;
-        # koirc.General.start-hidden=1;
+        kwalletrc.Wallet.Enabled = false;
       };
 
       immutableByDefault = false;

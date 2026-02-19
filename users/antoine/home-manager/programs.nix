778df6029bbc8f98b6dc08e3ebec26ceadb05253
Klbgr <qiuantoine@gmail.com>
2026-02-20 00:25:43 +0100

    Added AFFiNE

--- users/antoine/home-manager/programs.nix
+++ users/antoine/home-manager/programs.nix
@@ -62,6 +62,7 @@ in
     packet
     freecad
     orca-slicer
+    affine
   ];
 
   dconf.settings = {
@@ -161,6 +162,11 @@ in
       Name = "Packet";
       Exec = "packet -b";
     })
+    (mkDesktopFile "AFFiNE" {
+      Type = "Application";
+      Name = "AFFiNE";
+      Exec = "affine";
+    })
 
     {
       "easyeffects/db/easyeffectsrc".text = lib.generators.toINI { } {
@@ -174,4 +180,46 @@ in
       };
     }
   ];
+
+  home.activation.mergeAffineConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    merge_json() {
+      local DEST_FILE="$1"
+      local WANTED_JSON="$2"
+      local DIR
+      DIR=$(dirname "$DEST_FILE")
+
+      if [ ! -d "$DIR" ]; then
+        mkdir -p "$DIR"
+      fi
+
+      if [ -f "$DEST_FILE" ]; then
+        local TEMP_FILE
+        TEMP_FILE=$(mktemp)
+        ${pkgs.jq}/bin/jq -s '.[0] * .[1]' "$DEST_FILE" <(echo "$WANTED_JSON") > "$TEMP_FILE"
+        mv "$TEMP_FILE" "$DEST_FILE"
+      else
+        echo "$WANTED_JSON" > "$DEST_FILE"
+      fi
+    }
+
+    STATE_FILE="$HOME/.config/AFFiNE/global-state.json"
+    CACHE_FILE="$HOME/.config/AFFiNE/global-cache.json"
+
+    WANTED_STATE='{
+      "menubarState": {
+        "closeToTray": true,
+        "enabled": true,
+        "minimizeToTray": true,
+        "openOnLeftClick": true,
+        "startMinimized": true
+      }
+    }'
+
+    WANTED_CACHE='{
+      "i18n_lng": "fr"
+    }'
+
+    merge_json "$STATE_FILE" "$WANTED_STATE"
+    merge_json "$CACHE_FILE" "$WANTED_CACHE"
+  '';
 }

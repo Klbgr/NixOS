29f7c3c228e3d04345c40a1942e1afb8f22b780a
Klbgr <qiuantoine@gmail.com>
2026-02-22 23:20:44 +0100

    Added FileLight configuration

--- users/antoine/modules/kde.nix
+++ users/antoine/modules/kde.nix
@@ -101,6 +101,15 @@
                 timezone = "Europe/Paris";
               };
             };
+            filelightrc.filelight_part = {
+              contrast = 75;
+              scanAcrossMounts = false;
+              scanRemoteMounts = false;
+              scheme = 1;
+              showFoldersSidebar = true;
+              showSmallFiles = false;
+              skipList = "/dev,/proc,/sys,/root,/mnt";
+            };
           };
 
           immutableByDefault = false;

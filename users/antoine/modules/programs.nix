b57ca6b6127aeac9101b633df526c30e0e2d4056
Klbgr <qiuantoine@gmail.com>
2026-03-15 15:30:04 +0100

    feat: Added Speedtest configuration

--- users/antoine/modules/programs.nix
+++ users/antoine/modules/programs.nix
@@ -187,6 +187,14 @@
               showTrayIcon = false;
             };
           };
+          "ookla/speedtest-cli.json".text = ''
+            {
+                "Settings": {
+                    "LicenseAccepted": "604ec27f828456331ebf441826292c49276bd3c1bee1a2f65a6452f505c4061c",
+                    "GDPRTimeStamp": 1773584199
+                }
+            }
+          '';
         }
       ];
 

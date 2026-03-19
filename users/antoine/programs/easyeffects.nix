5f94438c23933e5aeb78e8c96bb06f0fff7b4545
Klbgr <qiuantoine@gmail.com>
2026-03-19 22:46:02 +0100

    feat: Add EasyEffects "Advanced Auto Gain" preset and configure it to load on startup.

--- users/antoine/programs/easyeffects.nix
+++ users/antoine/programs/easyeffects.nix
@@ -3,15 +3,23 @@
 {
   home-manager.users.antoine =
     { lib, pkgs, ... }:
+    let
+      presets-src = pkgs.fetchFromGitHub {
+        owner = "JackHack96";
+        repo = "EasyEffects-presets";
+        rev = "master";
+        sha256 = "sha256-9lSYaWGIQ9K53NwQULmbdDxnS4NijmnOEUvFQWjEF08=";
+      };
+    in
     {
       home.packages = with pkgs; [
         easyeffects
       ];
 
       xdg.configFile = {
+        "easyeffects/output/Advanced Auto Gain.json".source = "${presets-src}/Advanced Auto Gain.json";
         "easyeffects/db/easyeffectsrc".text = lib.generators.toINI { } {
           StreamOutputs = {
-            plugins = "bass_enhancer#0,crystalizer#0";
             visiblePage = "pluginsPage";
           };
           Window = {
@@ -22,7 +30,7 @@
           [Desktop Entry]
           Type = Application
           Name = Easy Effects
-          Exec = easyeffects --hide-window
+          Exec = easyeffects --hide-window & easyeffects --load-preset "Advanced Auto Gain"
         '';
       };
     };

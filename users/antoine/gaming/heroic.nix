d6f1b733945e1af1c19c7f4485b3a58d488d7b85
Klbgr <qiuantoine@gmail.com>
2026-03-22 17:30:56 +0100

    feat: Add Heroic Games Launcher configuration patching and remove Lutris.

--- users/antoine/gaming/heroic.nix
+++ users/antoine/gaming/heroic.nix
@@ -2,11 +2,58 @@
 
 {
   home-manager.users.antoine =
-    { pkgs, ... }:
+    { lib, pkgs, ... }:
 
     {
       home.packages = with pkgs; [
         heroic
       ];
+
+      home.activation.mergeHeroicConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        PATCHER="${pkgs.configuration-patcher}/bin/configuration-patcher"
+
+        CONFIG_FILE="$HOME/.config/heroic/config.json"
+
+        $PATCHER json "$CONFIG_FILE" '{
+          "defaultSettings": {
+            "language": "fr",
+            "checkForUpdatesOnStartup": false,
+            "autoUpdateGames": false,
+            "enableUpdates": false,
+            "hideChangelogsOnStartup": true,
+            "noTrayIcon": false,
+            "exitToTray": true,
+            "startInTray": false,
+            "minimizeOnLaunch": true,
+            "darkTrayIcon": false,
+            "framelessWindow": false,
+            "addDesktopShortcuts": false,
+            "addStartMenuShortcuts": false,
+            "addSteamShortcuts": true,
+            "discordRPC": true,
+            "downloadProtonToSteam": false,
+            "analyticsOptIn": false,
+            "libraryTopSection": "disabled",
+            "maxWorkers": 0,
+            "autoInstallDxvkNvapi": true,
+            "enableEsync": true,
+            "enableFsync": true,
+            "enableWineWayland": false,
+            "enableWoW64": false,
+            "showFps": false,
+            "showMangohud": true,
+            "useGameMode": true,
+            "useSteamRuntime": true,
+            "battlEyeRuntime": true,
+            "eacRuntime": true,
+            "showValveProton": false,
+            "experimentalFeatures": {
+                "enableHelp": false,
+                "cometSupport": false,
+                "zoomPlatform": false
+            }
+          }
+        }' 
+      '';
     };
 }

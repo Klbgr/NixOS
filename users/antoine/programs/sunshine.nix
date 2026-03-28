c51eda01e6684cc5690e0259586349c05d8edf40
Klbgr <qiuantoine@gmail.com>
2026-03-28 14:54:06 +0100

    feat: Configure sunshine applications.

--- users/antoine/programs/sunshine.nix
+++ users/antoine/programs/sunshine.nix
@@ -1,20 +1,38 @@
 { ... }:
 
 {
-  home-manager.users.antoine =
-    { ... }:
-
-    {
-      xdg.configFile."sunshine/sunshine.conf".text = ''
-        locale = fr
-        system_tray = disabled
-      '';
-    };
-
   services.sunshine = {
     enable = true;
+    settings = {
+      locale = "fr";
+      system_tray = "disabled";
+    };
     autoStart = true;
     capSysAdmin = true;
     openFirewall = true;
+    applications = {
+      env = {
+        PATH = "$(PATH):$(HOME)/.local/bin";
+      };
+      apps = [
+        {
+          name = "Desktop";
+          "image-path" = "desktop.png";
+        }
+        {
+          name = "Steam Big Picture";
+          detached = [
+            "setsid steam steam://open/bigpicture"
+          ];
+          "prep-cmd" = [
+            {
+              do = "";
+              undo = "setsid steam steam://close/bigpicture";
+            }
+          ];
+          "image-path" = "steam.png";
+        }
+      ];
+    };
   };
 }

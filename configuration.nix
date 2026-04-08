0bf968d2734376c8489a547279a34615ac218ba6
Klbgr <qiuantoine@gmail.com>
2026-04-08 16:33:50 +0200

    feat: add power management services

--- configuration.nix
+++ configuration.nix
@@ -132,6 +132,10 @@
 
   services.fwupd.enable = true;
 
+  services.power-profiles-daemon.enable = true;
+
+  services.upower.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave

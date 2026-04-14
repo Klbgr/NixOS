dd4871565bb1cb58e98a5869126b7a95fa694393
Klbgr <qiuantoine@gmail.com>
2026-04-14 21:43:38 +0200

    feat: add exfatprogs to partition manager configuration

--- users/antoine/kde/kde-packages/partition-manager.nix
+++ users/antoine/kde/kde-packages/partition-manager.nix
@@ -2,4 +2,13 @@
 
 {
   programs.partition-manager.enable = true;
+
+  home-manager.users.antoine =
+    { pkgs, ... }:
+
+    {
+      home.packages = with pkgs; [
+        exfatprogs
+      ];
+    };
 }

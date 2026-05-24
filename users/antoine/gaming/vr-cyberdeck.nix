23b939f2e500b983cd53a913767deee096f7d5d2
Klbgr <qiuantoine@gmail.com>
2026-05-24 16:14:24 +0200

    feat: replace apprenticevr with vr-cyberdeck

---
+++ users/antoine/gaming/vr-cyberdeck.nix
@@ -0,0 +1,42 @@
+{ ... }:
+
+{
+  home-manager.users.antoine =
+    { pkgs, ... }:
+    let
+      vr-cyberdeck = pkgs.appimageTools.wrapType2 rec {
+        pname = "vr-cyberdeck";
+        version = "1.2.3";
+        src = pkgs.fetchurl {
+          url = "https://github.com/KaladinDMP/VR-CyberDeck/releases/download/v${version}/vr-cyberdeck-${version}-x86_64.AppImage";
+          hash = "sha256-+4rWUS3L5nA+Y7mrlWSrkQgUbHy0IiHv8C5jtBIknFc=";
+        };
+
+        extraInstallCommands = ''
+          install -m 444 -D ${
+            pkgs.fetchurl {
+              url = "https://raw.githubusercontent.com/KaladinDMP/VR-CyberDeck/refs/heads/main/resources/icon.svg";
+              hash = "sha256-nC201nrGylrjeXqTVmGGsJHuIwiMb7Wsio7ZhXOWTjU=";
+            }
+          } $out/share/icons/hicolor/512x512/apps/vr-cyberdeck.svg
+
+          mkdir -p $out/share/applications
+          cp ${
+            pkgs.makeDesktopItem {
+              name = "vr-cyberdeck";
+              desktopName = "VR CyberDeck";
+              exec = "vr-cyberdeck";
+              icon = "vr-cyberdeck";
+              categories = [ "Game" ];
+              comment = "A cyberpunk flavored, multi OS, sideloader for the Meta Quest devices";
+            }
+          }/share/applications/*.desktop $out/share/applications/
+        '';
+      };
+    in
+    {
+      home.packages = [
+        vr-cyberdeck
+      ];
+    };
+}

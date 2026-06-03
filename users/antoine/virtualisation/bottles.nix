bb0511f54a3fab4d1f316d88b0cf5d1a2457f9e2
Klbgr <qiuantoine@gmail.com>
2026-06-03 21:19:23 +0200

    chore: update flake inputs and remove redundant package override

--- users/antoine/virtualisation/bottles.nix
+++ users/antoine/virtualisation/bottles.nix
@@ -3,19 +3,10 @@
 {
   home-manager.users.antoine =
     { pkgs, ... }:
-    #TODO remove once fixed in nixpkgs
-    let
-      pkgsFixed = pkgs.extend (
-        final: prev: {
-          openldap = prev.openldap.overrideAttrs (oldAttrs: {
-            doCheck = false;
-          });
-        }
-      );
-    in
+
     {
       home.packages = with pkgs; [
-        (pkgsFixed.bottles.override {
+        (bottles.override {
           removeWarningPopup = true;
         })
       ];

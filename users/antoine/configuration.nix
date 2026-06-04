14756b6c54ec16590ede0a82978d4f5b94f5a9a0
Klbgr <qiuantoine@gmail.com>
2026-06-04 21:26:45 +0200

    feat: add apply-theme alias to home-manager configuration

--- users/antoine/configuration.nix
+++ users/antoine/configuration.nix
@@ -33,28 +33,32 @@
     ];
   };
 
-  home-manager.users.antoine = {
-    programs.bash = {
-      enable = true;
-      shellAliases = {
-        rebuild = "sudo nixos-rebuild switch --flake /etc/nixos/";
-        dry-run = "sudo nixos-rebuild dry-run --flake /etc/nixos/";
-        configure = "code /etc/nixos";
-        clean = "sudo nix-collect-garbage -d && nix-collect-garbage -d";
-        optimise = "nix-store --optimise";
+  home-manager.users.antoine =
+    { config, ... }:
+
+    {
+      programs.bash = {
+        enable = true;
+        shellAliases = {
+          rebuild = "sudo nixos-rebuild switch --flake /etc/nixos/";
+          dry-run = "sudo nixos-rebuild dry-run --flake /etc/nixos/";
+          configure = "code /etc/nixos";
+          clean = "sudo nix-collect-garbage -d && nix-collect-garbage -d";
+          optimise = "nix-store --optimise";
+          apply-theme = "${config.home.homeDirectory}/.local/share/plasma-manager/run_all.sh";
+        };
+      };
+
+      xdg.userDirs = {
+        enable = true;
+        createDirectories = true;
+        setSessionVariables = true;
+      };
+
+      # The state version is required and should stay at the version you
+      # originally installed.
+      home = {
+        stateVersion = "25.11";
       };
     };
-
-    xdg.userDirs = {
-      enable = true;
-      createDirectories = true;
-      setSessionVariables = true;
-    };
-
-    # The state version is required and should stay at the version you
-    # originally installed.
-    home = {
-      stateVersion = "25.11";
-    };
-  };
 }

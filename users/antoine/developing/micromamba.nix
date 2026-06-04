3786977fe75aeb9feb4031795a364bf7b18b4c9c
Klbgr <qiuantoine@gmail.com>
2026-06-04 16:18:51 +0200

    refactor: update micromamba

--- users/antoine/developing/micromamba.nix
+++ users/antoine/developing/micromamba.nix
@@ -20,14 +20,14 @@
       programs.bash = {
         bashrcExtra = ''
           # >>> mamba initialize >>>
-          # !! Contents within this block are managed by 'micromamba shell init' !!
-          export MAMBA_EXE='${pkgs.micromamba}/bin/micromamba';
+          # !! Contents within this block are managed by '.mamba-wrapped shell init' !!
+          export MAMBA_EXE='${pkgs.micromamba}/bin/.mamba-wrapped';
           export MAMBA_ROOT_PREFIX='${config.home.homeDirectory}/.local/share/mamba';
           __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
           if [ $? -eq 0 ]; then
               eval "$__mamba_setup"
           else
-              alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
+              alias .mamba-wrapped="$MAMBA_EXE"  # Fallback on help from .mamba-wrapped activate
           fi
           unset __mamba_setup
           # <<< mamba initialize <<<

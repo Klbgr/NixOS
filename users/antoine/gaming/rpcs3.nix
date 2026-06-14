1e4585d742ea122d2d92424d09546dd8e8dc1252
Klbgr <qiuantoine@gmail.com>
2026-06-14 16:10:41 +0200

    feat: add Heroic sideloaded games library management and migrate RPCS3 to flake-based inputs

--- users/antoine/gaming/rpcs3.nix
+++ users/antoine/gaming/rpcs3.nix
@@ -1,23 +1,22 @@
-{ ... }:
-
+{ inputs, pkgs, ... }:
+let
+  rpcs3-nixpkgs = import inputs.rpcs3-nixpkgs {
+    inherit (pkgs) system;
+  };
+in
 {
   home-manager.users.antoine =
     { pkgs, lib, ... }:
-    let
-      pinnedPkgs = import (fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/1b06bb58c32597211752f4775e14b243485a374d.tar.gz";
-        sha256 = "0mjx7fd8g3f8ayshaaq9xwb43kgizgqp9wnll8f2kdrq0fawxrbc";
-      }) { inherit (pkgs) system; };
-    in
+
     {
       home.packages = with pkgs; [
         (symlinkJoin {
           name = "rpcs3-with-mangohud";
-          paths = [ pinnedPkgs.rpcs3 ];
+          paths = [ rpcs3-nixpkgs.rpcs3 ];
           nativeBuildInputs = [ makeWrapper ];
           postBuild = ''
             wrapProgram $out/bin/rpcs3 \
-              --run "${mangohud}/bin/mangohud ${pinnedPkgs.rpcs3}/bin/rpcs3; exit 0"
+              --run "${mangohud}/bin/mangohud ${rpcs3-nixpkgs.rpcs3}/bin/rpcs3; exit 0"
           '';
         })
       ];

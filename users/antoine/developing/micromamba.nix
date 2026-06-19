43e392726c2d8796c0e8abe0c4f73ae3dd1e138c
Klbgr <qiuantoine@gmail.com>
2026-06-20 00:01:42 +0200

    feat: switch from micromamba to conda

--- users/antoine/developing/micromamba.nix
+++ users/antoine/developing/micromamba.nix
@@ -1,62 +1,12 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   home-manager.users.antoine =
-    { config, pkgs, ... }:
+    { pkgs, ... }:
 
     {
       home.packages = with pkgs; [
-        micromamba
+        conda
       ];
-
-      home.file.".mambarc".text = ''
-        auto_activate_base: false
-        envs_dirs:
-          - ${config.home.homeDirectory}/.mamba/envs
-        pkgs_dirs:
-          - ${config.home.homeDirectory}/.mamba/pkgs
-      '';
-
-      programs.bash = {
-        bashrcExtra = ''
-          # >>> mamba initialize >>>
-          # !! Contents within this block are managed by '.mamba-wrapped shell init' !!
-          export MAMBA_EXE='${pkgs.micromamba}/bin/.mamba-wrapped';
-          export MAMBA_ROOT_PREFIX='${config.home.homeDirectory}/.local/share/mamba';
-          __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-          if [ $? -eq 0 ]; then
-              eval "$__mamba_setup"
-          else
-              alias .mamba-wrapped="$MAMBA_EXE"  # Fallback on help from .mamba-wrapped activate
-          fi
-          unset __mamba_setup
-          # <<< mamba initialize <<<
-        '';
-        shellAliases = {
-          conda = "micromamba";
-          mamba = "micromamba";
-        };
-      };
     };
-
-  programs.nix-ld = {
-    enable = true;
-    libraries = with pkgs; [
-      zlib
-      zstd
-      stdenv.cc.cc
-      curl
-      openssl
-      attr
-      libssh
-      bzip2
-      libxml2
-      acl
-      libsodium
-      util-linux
-      xz
-      systemd
-      libxcb
-    ];
-  };
 }

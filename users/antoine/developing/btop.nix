d39b5801e8e6f09eff3e914b709b10a860943ad5
Klbgr <qiuantoine@gmail.com>
2026-06-28 15:03:47 +0200

    feat: enable GPU support for btop

--- users/antoine/developing/btop.nix
+++ users/antoine/developing/btop.nix
@@ -2,11 +2,15 @@
 
 {
   home-manager.users.antoine =
-    { ... }:
+    { pkgs, ... }:
 
     {
       programs.btop = {
         enable = true;
+        package = pkgs.btop.override {
+          cudaSupport = true;
+          rocmSupport = true;
+        };
       };
     };
 }
